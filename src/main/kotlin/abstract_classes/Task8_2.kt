6fde7f0bd30089791832ee8b161fad9183d2faaf
Мугинов Тимур <tmigunov@intelogis.ru>
2024-10-02 10:37:27 +0300

    add 8_2

--- src/main/kotlin/abstract_classes/Task8_2.kt
+++ src/main/kotlin/abstract_classes/Task8_2.kt
@@ -1,6 +1,20 @@
-package abstract_classes/*
+package abstract_classes
+
+import kotlin.math.sqrt
+
+/*
 Добавить еще один класс Triangle – тоже наследник Figure (который вы сделали в предыдущем задании)
 и прописать работу функций area() и length() для него
 
 Для этой задачи есть видео с разбором.
 */
+class Triangle(color: String, val a: Double, val b: Double, val c: Double): Figure(color){
+    override fun area(): Double {
+        val p=(a+b+c)/2
+        return sqrt(p*(p-a)*(p-b)*(p-c))
+    }
+
+    override fun length(): Double {
+        return a+b+c
+    }
+}
